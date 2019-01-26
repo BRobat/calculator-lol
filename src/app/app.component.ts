b553d2545f8a583eef4cba02be318ba1aaaf23cb
BRobat <bartlomiej.robat@gmail.com>
2019-01-26 17:56:06 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculator';
+
+  value = "";
+  history = [];
+
+  append(value) {
+    this.value += value;
+  }
+  
+  result() {
+    this.history.unshift(this.value);
+
+    let numbers = [];
+    let operations = [];
+    let val = this.value.split(' ');
+
+    for(let i = 0; i < val.length; i++) {
+      
+      try {
+        numbers.push(parseInt(val[i]))
+      } catch (error) {
+      }
+
+    }
+
+    console.log(numbers,operations)
+
+  }
+
+  clear() {
+    this.value = "";
+  }
+  
+  rpo(numArray, opArray) {
+    //reverse polish notation
+
+  }
+
+  
+
+  add(a,b) {
+    return a + b;
+  }
+
+  sub(a,b) {
+    return a - b;
+  }
+
+  mul(a,b) {
+    return a * b;
+  }
+
+  div(a,b) {
+    return a / b;
+  }
+
 }
+
+
+/*
+TODO:
+get value
+turn it into array of numbers and symbols V
+then use reverse polish notation
+calculate
+
+
+
+*/
