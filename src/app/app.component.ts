af358f244fa60a8eaf5390eaa6dd31e886e5c599
BRobat <bartlomiej.robat@gmail.com>
2019-01-27 13:47:29 +0100

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,43 +8,94 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  value = "";
+  displayNum1 = "";
+  displayNum2 = "";
+  displayOp = "";
   history = [];
 
-  append(value) {
-    this.value += value;
+  opActive = false;
+
+  appendNum(value) {
+    if (this.opActive) {
+      this.displayNum2 += value;
+    } else {
+      this.displayNum1 += value;
+    }
+  }
+
+  appendOp(value) {
+    this.opActive = true;
+    if (this.displayOp == "") {
+      this.displayOp = value;
+    }
   }
   
   result() {
-    this.history.unshift(this.value);
+    let result: number;
+    let num1 = parseInt(this.displayNum1);
+    let num2 = parseInt(this.displayNum2);
 
-    let numbers = [];
-    let operations = [];
-    let val = this.value.split(' ');
-
-    for(let i = 0; i < val.length; i++) {
-      
-      try {
-        numbers.push(parseInt(val[i]))
-      } catch (error) {
-      }
-
+    if (isNaN(num2)) {
+      num2 = 0;
     }
 
-    console.log(numbers,operations)
+    switch(this.displayOp) {
+      case "": {
+        result = num1;
+        break;
+      }
+      case "+": {
+        result = this.add(num1,num2);
+        break;
+      }
+      case "-": {
+        result = this.sub(num1,num2);
+        break;
+      }
+      case "*": {
+        result = this.mul(num1,num2);
+        break;
+      }
+      case "/": {
+        result = this.div(num1,num2);
+        break;
+      }
+      case "^": {
+        result = this.pow(num1,num2);
+        break;
+      }
+    }
 
+    result = this.toInt(result);
+    if (isNaN(result)){
+      result = 0; 
+    } 
+
+    let opStr = num1 + " " + this.displayOp + " " + num2 + " = " + result;
+    this.history.unshift(opStr);
+    this.clear();
+    
+    this.displayNum1 = String(result);
+  }
+
+  back() {
+    if (this.opActive) {
+      this.displayNum2 = this.displayNum2.substring(0, this.displayNum2.length - 1);
+    } else {
+      this.displayNum1 = this.displayNum1.substring(0, this.displayNum1.length - 1);
+    }
   }
 
   clear() {
-    this.value = "";
-  }
-  
-  rpo(numArray, opArray) {
-    //reverse polish notation
-
+    this.displayNum1 = "";
+    this.displayNum2 = "";
+    this.displayOp = "";
+    this.opActive = false;
   }
 
-  
+  clearHistory() {
+    this.history = [""];
+  }
 
   add(a,b) {
     return a + b;
@@ -63,16 +113,11 @@ export class AppComponent {
     return a / b;
   }
 
+  pow(a,b) {
+    return a ** b;
+  }
+
+  toInt(a) {
+    return a - (a%1)
+  }
 }
-
-
-/*
-TODO:
-get value
-turn it into array of numbers and symbols V
-then use reverse polish notation
-calculate
-
-
-
-*/
